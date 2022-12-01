cc3f57dabf143b8d5535eed76f1c7b804e346fa5
Mario <mmerayo1803@gmail.com>
2022-12-01 12:20:45 +0100

    primer commit

---
+++ comprobador.sh
@@ -0,0 +1,69 @@
+FILE=Api.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=a.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=b.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=config/config.bck
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=config/config.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=config.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=Controller.java
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=c.tmp
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/config/values.dat
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data01.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data02.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data03.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/data04.csv
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
+
+FILE=data1/init.txt
+if [ ! -f "$FILE" ]; then
+    echo "ERROR! $FILE no existe!"
+fi
